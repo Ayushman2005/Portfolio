51fc6e9dd72d3eddb9b18293d2ae1cc9ee3f40ab
Ayushman2005 <karayushman736@gmail.com>
2026-03-09 15:52:43 +0530

    09/03/2026

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,23 +6,23 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:10000',
         changeOrigin: true,
       },
       '/static': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:10000',
         changeOrigin: true,
       },
       '/login': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:10000',
         changeOrigin: true,
       },
       '/admin': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:10000',
         changeOrigin: true,
       },
       '/logout': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:10000',
         changeOrigin: true,
       }
     }
